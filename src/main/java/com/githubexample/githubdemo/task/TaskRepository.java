6dd3831055bae72445734dd31edf73ec56dabaa4
ananthintk-lab <ananthintk@gmail.com>
2026-06-30 21:16:11 +0530

    feat: [US-001] - Add priority field to database

---
+++ src/main/java/com/githubexample/githubdemo/task/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.githubexample.githubdemo.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByPriority(Priority priority);
+}
